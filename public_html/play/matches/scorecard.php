ec23f468f82eda081fc18649e48bcd550341a08b
Rick Mason <rick@stoolball.org.uk>
2016-01-22 17:18:46 +0000

    Separate pages for fixture and scorecard editing

--- public_html/play/matches/scorecard.php
+++ public_html/play/matches/scorecard.php
@@ -1,10 +1,9 @@
 <?php
 ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT'] . '/../classes/');
 
-# include required functions
 require_once('page/stoolball-page.class.php');
 require_once('stoolball/match-manager.class.php');
-require_once('stoolball/matches/match-highlights-edit-control.class.php');
+require_once('stoolball/match-edit-control.class.php');
 
 class CurrentPage extends StoolballPage
 {
@@ -18,7 +17,7 @@ class CurrentPage extends StoolballPage
 	/**
 	 * Editor for the match
 	 *
-	 * @var MatchHighlightsEditControl
+	 * @var MatchEditControl
 	 */
 	private $editor;
 
@@ -40,7 +39,10 @@ class CurrentPage extends StoolballPage
 		$this->match_manager = new MatchManager($this->GetSettings(), $this->GetDataConnection());
 
 		# new edit control
-		$this->editor = new MatchHighlightsEditControl($this->GetSettings());
+		$this->editor = new MatchEditControl($this->GetSettings());
+        if (!$this->IsPostback()) {
+            $this->editor->SetCurrentPage(MatchEditControl::FIRST_INNINGS);
+        }
 		$this->RegisterControlForValidation($this->editor);
 
 		# check permissions
@@ -56,19 +58,22 @@ class CurrentPage extends StoolballPage
 		# Get the submitted match
 		$this->match = $this->editor->GetDataObject();
 
-		# Because this is a new request, we need to reverify whether this is the match owner before letting anything happen. 
-		# Can't trust that info from a postback so MUST go to the database to check it.
+        # Because this is a new request, we need to reverify whether this is the match owner before letting anything happen. 
+        # Can't trust that info from a postback so MUST go to the database to check it.
 		$this->match_manager->ReadByMatchId(array($this->editor->GetDataObjectId()));
 		$check_match = $this->match_manager->GetFirst();
 		$this->b_user_is_match_owner = ($check_match instanceof Match and AuthenticationManager::GetUser()->GetId() == $check_match->GetAddedBy()->GetId());
 
-		# Don't wan't to edit tournaments on this page, so even as admin make sure we're not trying to save one.
-		# Can't change the match type, so find out what that is from the db too. 
-		$this->b_is_tournament = ($check_match->GetMatchType() == MatchType::TOURNAMENT);
+        # Don't wan't to edit tournaments on this page, so even as admin make sure we're not trying to save one.
+        # Can't change the match type, so find out what that is from the db too. 
+        $this->b_is_tournament = ($check_match->GetMatchType() == MatchType::TOURNAMENT);
 
 		# Check whether cancel was clicked
 		if ($this->editor->CancelClicked())
 		{
+			# Match may have been updated so send an email
+			$this->match_manager->NotifyMatchModerator($this->match->GetId());
+
 			# If so, get the match's short URL and redirect
 			$this->match_manager->ExpandMatchUrl($this->match);
 			$this->Redirect($this->match->GetNavigateUrl());
@@ -77,35 +82,23 @@ class CurrentPage extends StoolballPage
 		# save data if valid
 		if($this->IsValid() and !$this->b_is_tournament)
 		{
-			# Save player of match
-			$this->match_manager->SaveHighlights($this->match);
-            $this->match_manager->ExpandMatchUrl($this->match);
-
-			# Add comment if provided
-			if (trim($this->match->GetNewComment()))
+			switch ($this->editor->GetCurrentPage())
 			{
-				require_once('forums/topic-manager.class.php');
-				require_once('forums/review-item.class.php');
-				require_once('forums/subscription-manager.class.php');
-				$topic_manager = new TopicManager($this->GetSettings(), $this->GetDataConnection());
+				case MatchEditControl::FIRST_INNINGS:
 
-				$item_to_comment_on = new ReviewItem($this->GetSettings());
-				$item_to_comment_on->SetType(ContentType::STOOLBALL_MATCH);
-				$item_to_comment_on->SetId($this->match->GetId());
-                $item_to_comment_on->SetNavigateUrl("https://" . $this->GetSettings()->GetDomain() . $this->match->GetNavigateUrl());
-				$message = $topic_manager->SaveComment($item_to_comment_on, $this->match->GetNewComment());
+					$this->match_manager->SaveScorecard($this->match, true, $this->SearchIndexer());
+					$this->editor->SetCurrentPage(MatchEditControl::SECOND_INNINGS);
 
-				# send subscription emails
-				$subs_manager = new SubscriptionManager($this->GetSettings(), $this->GetDataConnection());
-				$subs_manager->SendCommentsSubscriptions($item_to_comment_on, $message);
-				unset($subs_manager);
+					break;
+
+				case MatchEditControl::SECOND_INNINGS:
+
+					$this->match_manager->SaveScorecard($this->match, false, $this->SearchIndexer());
+                    $this->match_manager->ExpandMatchUrl($this->match);
+					http_response_code(303);
+					$this->Redirect($this->match->EditHighlightsUrl());
+					break;
 			}
-
-			# Match may have been updated so send an email
-			$this->match_manager->NotifyMatchModerator($this->match->GetId());
-
-			# Show user the match, so they can see update was applied
-			$this->Redirect($this->match->GetNavigateUrl());
 		}
 	}
 
@@ -113,17 +106,12 @@ class CurrentPage extends StoolballPage
 	{
 
 		/* @var $match_manager MatchManager */
-		/* @var $editor MatchEditControl */
 
 		# get id of Match
 		$i_id = $this->match_manager->GetItemId();
 
-		# If there's no id, ensure no match object is created. Page will then display a "match not found" message.
-		# There's a separate page for adding matches, even for admins.
-		if (!$i_id) return;
-
 		# Get details of match but, if invalid, don't replace submitted details with saved ones
-		if ($this->IsValid())
+		if ($i_id and $this->IsValid())
 		{
 			$this->match_manager->ReadByMatchId(array($i_id));
             $this->match_manager->ExpandMatchScorecards();
@@ -138,7 +126,10 @@ class CurrentPage extends StoolballPage
         
         # Tournament or match in the future or not played is page not found
         $editable_results = array(MatchResult::UNKNOWN, MatchResult::HOME_WIN, MatchResult::AWAY_WIN, MatchResult::TIE, MatchResult::ABANDONED);
-        if ($this->b_is_tournament or $this->match->GetStartTime() > gmdate('U') or !in_array($this->match->Result()->GetResultType(), $editable_results))
+        if (!$this->match instanceof Match or 
+            $this->b_is_tournament or 
+            $this->match->GetStartTime() > gmdate('U') 
+            or !in_array($this->match->Result()->GetResultType(), $editable_results))
         {
             http_response_code(404);
             $this->page_not_found  = true;
@@ -149,12 +140,8 @@ class CurrentPage extends StoolballPage
 	{
 		/* @var $match Match */
 		$this->SetContentConstraint(StoolballPage::ConstrainText());
-
-		if (!$this->match instanceof Match)
-		{
-			$this->SetPageTitle('Match not found');
-			return; # Don't load any JS
-		}
+        $this->SetContentCssClass('scorecardPage');
+        $this->SetContentCssClass('scorecardPage');
 
         if ($this->page_not_found)
         {
@@ -163,13 +150,11 @@ class CurrentPage extends StoolballPage
         }
 
 		# Set page title
-        $edit_or_update = ($this->b_user_is_match_admin or $this->b_user_is_match_owner) ? "Edit" : "Update";
-		$this->SetPageTitle("$edit_or_update " . $this->match->GetTitle() . ', ' . Date::BritishDate($this->match->GetStartTime()) . ", step 4 of 4");
+		$edit_or_update = ($this->b_user_is_match_admin or $this->b_user_is_match_owner) ? "Edit" : "Update";
+		$step = ", step " . $this->editor->GetCurrentPage() . " of 4";
+		$this->SetPageTitle("$edit_or_update " . $this->match->GetTitle() . ', ' . Date::BritishDate($this->match->GetStartTime()) . $step);
 
 		# Load JavaScript
-        $this->LoadClientScript("/scripts/tiny_mce/jquery.tinymce.js");
-        $this->LoadClientScript("/scripts/tinymce.js");
-
 		$autocomplete_team_ids = array();
 		if ($this->match->GetHomeTeamId()) $autocomplete_team_ids[] = $this->match->GetHomeTeamId();
 		if ($this->match->GetAwayTeamId()) $autocomplete_team_ids[] = $this->match->GetAwayTeamId();
@@ -187,22 +172,39 @@ class CurrentPage extends StoolballPage
 
 	public function OnPageLoad()
 	{
-		# There's a separate page for adding matches, even for admins.
-		if (!$this->match instanceof Match)
-		{
-			echo new XhtmlElement('p', "Sorry, we couldn't find the match you're trying to edit.");
-			return ;
-		}
-
-        # Matches this page shouldn't edit are page not found
+       # Matches this page shouldn't edit are page not found
         if ($this->page_not_found)
         {
            require_once($_SERVER['DOCUMENT_ROOT'] . "/wp-content/themes/stoolball/section-404.php");
            return;
         }
-        
-        $edit_or_update = ($this->b_user_is_match_admin or $this->b_user_is_match_owner) ? "Edit" : "Update";
-        echo new XhtmlElement('h1', "$edit_or_update " . htmlentities($this->match->GetTitle(), ENT_QUOTES, "UTF-8", false) . " &#8211; step 4 of 4");
+
+		$edit_or_update = ($this->b_user_is_match_admin or $this->b_user_is_match_owner) ? "Edit" : "Update";
+		$step = " &#8211; step " . $this->editor->GetCurrentPage() . " of 4";
+		echo new XhtmlElement('h1', "$edit_or_update " . htmlentities($this->match->GetTitle(), ENT_QUOTES, "UTF-8", false) . $step);
+
+		if ($this->IsValid())
+		{
+			/* Create instruction panel */
+			$panel_inner2 = new XhtmlElement('div');
+			$panel_inner1 = new XhtmlElement('div', $panel_inner2);
+			$panel = new XhtmlElement('div', $panel_inner1);
+			$panel->SetCssClass('panel instructionPanel');
+
+			$title_inner3 = new XhtmlElement('span', 'Fill in scorecards quickly:');
+			$title_inner2 = new XhtmlElement('span', $title_inner3);
+			$title_inner1 = new XhtmlElement('span', $title_inner2);
+			$title = new XhtmlElement('h2', $title_inner1, "large");
+			$panel_inner2->AddControl($title);
+
+			$tab_tip = new XhtmlElement('ul');
+			$tab_tip->AddControl(new XhtmlElement('li', 'Use the <span class="tab">tab</span> and up and down keys to move through the form', "large"));
+			$tab_tip->AddControl(new XhtmlElement('li', 'Use the <span class="tab">tab</span> key to select a player\'s name from the suggestions', "large"));
+            $tab_tip->AddControl(new XhtmlElement('li', "List everyone on the batting card, even if they didn't bat, so we know who played."));
+			$tab_tip->AddControl(new XhtmlElement('li', 'Don\'t worry if you don\'t know &#8211; fill in what you can and leave the rest blank.'));
+			$panel_inner2->AddControl($tab_tip);
+			echo $panel;
+		}
 
 		# OK to edit the match
 		$this->editor->SetDataObject($this->match);
