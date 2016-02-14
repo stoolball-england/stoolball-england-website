27e32109862230b6dc0febd40f00ad3b86cb67f9
Rick Mason <rick@stoolball.org.uk>
2016-02-14 16:59:40 +0000

    Update first draft poster design

--- public_html/play/posters/poster-pdf.php
+++ public_html/play/posters/poster-pdf.php
@@ -38,28 +38,19 @@ $pdf->SetAuthor('Stoolball England');
 $pdf->SetTitle('Play stoolball');
 $pdf->SetSubject('Play stoolball');
 
-// set default header data
-$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 001', PDF_HEADER_STRING, array(0,64,255), array(0,64,128));
-$pdf->setFooterData(array(0,64,0), array(0,64,128));
+// remove default header/footer
+$pdf->setPrintHeader(false);
+$pdf->setPrintFooter(false);
 
-// set header and footer fonts
-$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
-$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
 
 // set default monospaced font
 $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
 // set margins
-$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
-$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
-$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+$pdf->SetMargins(0,0,0);
 
 // set auto page breaks
-$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
-
-// set image scale factor
-$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
-
+$pdf->SetAutoPageBreak(false, 0);
 // ---------------------------------------------------------
 
 // set default font subsetting mode
@@ -75,17 +66,20 @@ $pdf->SetFont('dejavusans', '', 14, '', true);
 // This method has several options, check the source code documentation for more information.
 $pdf->AddPage();
 
+$pdf->Image('images/background.jpg', 0, 0, 210, 297, 'JPG', '', '', false);
+
+
 // Set some content to print
 $html = <<<EOD
-<h1>Welcome to <a href="http://www.tcpdf.org" style="text-decoration:none;background-color:#CC0000;color:black;">&nbsp;<span style="color:black;">TC</span><span style="color:white;">PDF</span>&nbsp;</a>!</h1>
-<i>This is the first example of TCPDF library.</i>
-<p>This text is printed using the <i>writeHTMLCell()</i> method but you can also use: <i>Multicell(), writeHTML(), Write(), Cell() and Text()</i>.</p>
-<p>Please check the source code documentation and other examples for further information.</p>
-<p style="color:#CC0000;">TO IMPROVE AND EXPAND TCPDF I NEED YOUR SUPPORT, PLEASE <a href="http://sourceforge.net/donate/index.php?group_id=128076">MAKE A DONATION!</a></p>
+<div style="color:#fff">
+<h1 style="margin:0">Anytown Stoolball Club</h1>
+<p style="margin:0">Tuesdays and Thursdays from May to August</p>
+<p style="margin:0">Call Jo Bloggs on 01234 567890 or email jo.bloggs@example.org</p>
+</div>
 EOD;
 
 // Print text using writeHTMLCell()
-$pdf->writeHTMLCell(0, 0, 100, 50, $html, 0, 1, 0, true, '', true);
+$pdf->writeHTMLCell(200, 100, 20, 235, $html, 0, 1, 0, true, '', true);
 
 // ---------------------------------------------------------
 
