36a6955f12620c6099967704429c5dd7e9571b29
Rick Mason <rick@stoolball.org.uk>
2016-02-27 21:19:55 +0000

    Add a second poster for mixed stoolball

---
+++ public_html/play/posters/designs/male-batsman.class.php
@@ -0,0 +1,25 @@
+<?php
+require_once("base-poster-design.class.php");
+
+class PosterDesign extends BasePosterDesign
+{
+    public function __construct(TCPDF $pdf, $title, $slogan, $name, $details) {
+            
+        $pdf->SetTextColor(255,255,255);
+        $pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>.5, 'depth_h'=>.5, 'color'=>array(0,0,0), 'opacity'=>.5, 'blend_mode'=>'Multiply'));
+
+        $pdf->SetFont('league-gothic', '', 100, '', true);
+        $pdf->writeHTMLCell(200, 200, 18, 35, $this->buildHTML('h1', $title), 0, 1, 0);
+        
+        $pdf->SetFont('AlegreyaSans-MediumItalic', '', 60, '', true);
+        $pdf->writeHTMLCell(60, 200, 140, 85, $this->buildHTML('p', $slogan, 65), 0, 1, 0);
+        
+        $pdf->setTextShadow(array('enabled'=>false));
+        $pdf->SetFont('AlegreyaSans-ExtraBold', '', 25, '', true);
+        $pdf->writeHTMLCell(180, 100, 18, 241, $this->buildHTML('h2', $name, 30), 0, 1, 0);
+        
+        $pdf->SetFont('AlegreyaSans-Regular', '', 16, '', true);
+        $pdf->writeHTMLCell(180, 100, 18, 254, $this->buildHTML('p', $details, 20), 0, 1, 0);
+    }
+}
+?>
