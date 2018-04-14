9a51dba3e3b049466d01f65d8942458f946be83c
Rick Mason <rick@stoolball.org.uk>
2018-04-14 11:09:02 +0100

    Remove Shirley Reed as an equipment supplier from the map

--- public_html/scripts/shop.js
+++ public_html/scripts/shop.js
@@ -16,7 +16,6 @@ $(function()
 
 	new google.maps.Marker({ position : new google.maps.LatLng(51.07064141136184, -0.31114161014556885), map : map, title : "Kay Price, Sussex County Stoolball Association" });
 	new google.maps.Marker({ position : new google.maps.LatLng(50.924040, -0.140333), map : map, title : "Trevor Parsons, Mid Sussex" });
-	new google.maps.Marker({ position : new google.maps.LatLng(51.0436658, -0.9561287), map : map, title : "Shirley Reed, Hampshire/West Sussex border" });
 	new google.maps.Marker({ position : new google.maps.LatLng(50.790485, 0.2655421), map : map, title : "Trina Perry, Eastbourne" });
 	new google.maps.Marker({ position : new google.maps.LatLng(50.9612347, 0.3018266), map : map, title : "Toni Wheatley, East Sussex" });
 	new google.maps.Marker({ position : new google.maps.LatLng(51.2103987, -0.3243005), map : map, title : "Julie Oliver, Surrey" }); // approx
